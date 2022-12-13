9282b730cc800101d6c6cf9477edad40ac564b65
BenYao21 <BenYao21@github.com>
2022-12-12 22:38:29 -0700

    primes edit 2

--- user/primes.c
+++ user/primes.c
@@ -2,30 +2,62 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
+#define MSGSIZE 36
+#define ZERO '0'
+#define ONE '1'
+
+void prime(int pipe_read, int pipe_write)
+{
+    char buf[MSGSIZE];
+    int val = 0;
+    read(pipe_read, buf, MSGSIZE);
+    for (int i = 0; i < MSGSIZE; i++)
+    {
+        if (buf[i] == ONE)
+        {
+            val = i;
+            break;
+        }
+    }
+    printf("prime %d", val);
+    buf[val] = ZERO;
+    int raw = val;
+    val += raw;
+    while (val < MSGSIZE)
+    {
+        buf[val] = ZERO;
+        val += raw;
+    }
+    int pid = fork();
+    if (pid > 0)
+    {
+        write(pipe_write, buf, MSGSIZE);
+    }
+    if (pid == 0)
+    {
+        prime(pipe_read, pipe_write);
+    }
+}
+
 int main(int argc, char *argv[])
 {
-    int p[2];
-    pipe(p);
-    for (int i = 2; i < 36; i++)
+    int fd[2];
+    pipe(fd);
+
+    int pid = fork();
+    char nums[MSGSIZE];
+    if (pid > 0)
     {
-        int cur = 2;
-        if (cur == i)
-        {
-            printf("prime %d", i);
-        }
-        else
-        {
-            if (i % cur == 0)
-            {
-                exit(0);
-            }
-            else
-            {
-                int nxt = cur + 1;
-                write(p[1], nxt, sizeof p[1]);
-                int fid = fork();
-            }
-        }
+        char nums[MSGSIZE];
+        nums[0] = ZERO;
+        nums[1] = ZERO;
+        write(fd[1], nums, MSGSIZE);
+    }
+
+    if (pid == 0)
+    {
+        prime(fd[0], fd[1]);
+        wait(0);
     }
     exit(0);
 }
